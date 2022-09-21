04c4cb170aac71ff76b985fe72952e8cf50dc113
Shah Rahman <srahman@verticalrelevance.com>
2022-09-21 11:14:32 -0400

    user data update

--- cdk_kafka/user_data.sh
+++ cdk_kafka/user_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yum update -y
 amazon-linux-extras install java-openjdk11 -y
-wget https://downloads.apache.org/kafka/3.2.1/kafka_2.13-3.2.1.tgz
-tar -xzf kafka_2.13-3.2.1.tgz
-kafka_2.13-3.2.1/bin/zookeeper-server-start.sh -daemon kafka_2.13-3.2.1/config/zookeeper.properties
+wget https://downloads.apache.org/kafka/3.2.3/kafka_2.13-3.2.3.tgz
+tar -xzf kafka_2.13-3.2.3.tgz
+kafka_2.13-3.2.3/bin/zookeeper-server-start.sh -daemon kafka_2.13-3.2.3/config/zookeeper.properties
 sleep 5s
-kafka_2.13-3.2.1/bin/kafka-server-start.sh -daemon kafka_2.13-3.2.1/config/server.properties
+kafka_2.13-3.2.3/bin/kafka-server-start.sh -daemon kafka_2.13-3.2.3/config/server.properties
